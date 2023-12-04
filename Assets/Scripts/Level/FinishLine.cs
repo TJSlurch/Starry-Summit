7e84a92e3213f31920af29a6198bafc54abcaa57
Toby Saunders <Toby.Saunders22@students.bhasvic.ac.uk>
2023-12-04 12:10:28 +0000

    tini

--- Assets/Scripts/Level/FinishLine.cs
+++ Assets/Scripts/Level/FinishLine.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
-    private int totalCollectibles = 10;
+    private int totalCollectibles = 6;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
